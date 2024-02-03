ec21a237c3f4bac3d55bbceb711c9c2b0edce35f
pavanpiyumantha44 <pavanpiyumantha222@gmail.com>
2024-02-03 18:03:49 +0530

    create test api

--- backend/Index.js
+++ backend/Index.js
@@ -1,19 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
+
 dotenv.config();
+
+const app = express();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO)
 .then(res=>{
     console.log("Database connected!!");
 })
 .catch(err=>{
-    console.log("Database us not connected!!");
+    console.log("Database is not connected!!");
+})
+
+app.use("/user",userRouter)
+
+app.get('/test',(req,res)=>{
+    res.json({message:"Hello World"})
 })
 
 
-const app = express();
-app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT,()=>{
